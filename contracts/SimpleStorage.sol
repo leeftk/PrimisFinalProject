c874f1350c0296df56e42d26169207b02d10c949
Lee Faria <>
2020-01-04 18:07:34 -0500

    Updated contract with getter function

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -4,14 +4,27 @@ contract SimpleStorage {
   uint storedData;
 
 bytes32 public proof;
-  
+
+uint public proofCount = 0;
+
+mapping(uint => proof) public proofs;
+
+
+ 
+
   
  // store a proof of existence in the contract state
     function notarize(string memory ipfshash ) public {
         proof = hash(ipfshash);
+        proofCount++;
+        proof = proofs[proofCount];
     }
     function hash(string memory ipfshash) internal view returns (bytes32){
       return sha256(abi.encodePacked(ipfshash));
+    }
+
+    function getProof(uint _proofcount) public view returns(string){
+      return proofs[_proofcount];
     }
 }
 
