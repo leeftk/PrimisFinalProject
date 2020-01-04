b5ad49df585777e9e4644b17625022a1d3580379
Lee Faria <>
2020-01-04 00:12:40 -0500

    updated contracts

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -15,21 +15,3 @@ bytes32 public proof;
     }
 }
 
-// // Proof of Existence contract, version 2
-// pragma solidity ^0.5.0;
-//   // state
-//   contract proofOfExistence{
-      
-
-//    bytes32 public proof;
-  
-  
-//  // store a proof of existence in the contract state
-//     function notarize(string memory ipfshash ) public {
-//         proof = hash(ipfshash);
-//     }
-//     function hash(string memory ipfshash) internal pure returns (bytes32){
-//       return sha256(abi.encodePacked(ipfshash));
-//     }
-//   // *transactional function*
-//   }
