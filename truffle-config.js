24e468b77acc56263ebd2ff7524ff852159a1bd7
Lee Faria <lee.lara1219@gmail.com>
2020-01-23 21:21:51 -0500

    added mythx & updated truffle-congig

--- truffle-config.js
+++ truffle-config.js
@@ -3,6 +3,7 @@ const path = require("path");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  plugins: [ "truffle-security" ],
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     development: {
